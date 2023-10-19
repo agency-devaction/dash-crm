0d5495f094fc424b89c973da793ece67416c0752
Alex Nogueira <alex@devaction.com.br>
2023-10-19 08:10:50 -0300

    wip: create route password recovery

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('login', Login::class)->name('login');
 Route::get('/registration', Register::class)->name('auth.register');
 Route::get('/logout', static fn () => Auth::logout())->name('auth.logout');
+Route::get('password/recovery', static fn () => 'Password Recovery')->name('password.recovery');
 
 Route::middleware('auth')->group(function () {
     Route::get('/', Welcome::class)->name('dashboard');
---
+++ tests/Feature/Auth/PassowrdRecovery.php
@@ -0,0 +1,9 @@
+<?php
+
+use function Pest\Laravel\get;
+
+test('needs to have a route to password recovery', function () {
+
+    get(route('password.recovery'))
+        ->assertOk();
+});
