323dfbf43979f9a7c91e59755c22f745c5da3ae0
Alex Nogueira <alex@devaction.com.br>
2023-10-19 07:01:20 -0300

    wip: create logout application

---
+++ app/Livewire/Auth/Logout.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Livewire\Auth;
+
+use Illuminate\Contracts\View\View;
+use Livewire\Component;
+
+class Logout extends Component
+{
+    public function render(): View
+    {
+        return view('livewire.auth.logout');
+    }
+
+    public function logout(): void
+    {
+        auth()->logout();
+        session()->regenerateToken();
+
+        $this->redirect(route('login'));
+    }
+}
