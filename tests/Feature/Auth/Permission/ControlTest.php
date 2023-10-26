e56f76d24103dce7421370cd82e9a19b4438b98b
Alex Nogueira <alex@devaction.com.br>
2023-10-26 07:27:49 -0300

    wip: create middleware protected admin routes

--- tests/Feature/Auth/Permission/ControlTest.php
+++ tests/Feature/Auth/Permission/ControlTest.php
@@ -3,7 +3,7 @@
 use App\Models\{Permission, User};
 use Database\Seeders\{PermissionSeeder, UsersSeeder};
 
-use function Pest\Laravel\{assertDatabaseHas, seed};
+use function Pest\Laravel\{actingAs, assertDatabaseHas, seed};
 
 it('should be able to give a user a permission to something', function () {
     $user = User::factory()->create();
@@ -34,4 +34,12 @@ test('permission has to have a seeder', function () {
         'user_id'       => User::first()?->getKey(),
         'permission_id' => Permission::query()->where('key', '=', 'be an admin')->first()?->getKey(),
     ]);
+});
+
+it('should block the access to ab admin page if the user does not have the permission be an admin', function () {
+    $user = User::factory()->create();
+
+    actingAs($user)
+        ->get(route('admin.dashboard'))
+        ->assertStatus(403);
 });
