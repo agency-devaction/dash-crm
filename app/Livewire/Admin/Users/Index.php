5eaf8e8b8361f5b0208b268263f36c5e71c3238c
Alex Nogueira <alex@devaction.com.br>
2023-10-29 21:18:58 -0300

    wip: let's create a livewire component to list all users in the page

---
+++ app/Livewire/Admin/Users/Index.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Livewire\Admin\Users;
+
+use App\Models\User;
+use Illuminate\Contracts\View\View;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Livewire\Attributes\Computed;
+use Livewire\Component;
+
+/**
+ * @property-read LengthAwarePaginator $users
+ */
+class Index extends Component
+{
+    public function render(): View
+    {
+        return view('livewire.admin.users.index');
+    }
+    #[Computed]
+    public function users(): LengthAwarePaginator
+    {
+        return User::paginate();
+    }
+}
--- tests/Feature/UserManagement/ListUserTest.php
+++ tests/Feature/UserManagement/ListUserTest.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Livewire\Admin\Users;
 use App\Models\User;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 use function Pest\Laravel\{actingAs, get};
 
@@ -16,4 +18,23 @@ test('make sure that route us protected by the permission BE_AN_ADMIN', function
 
     get(route('admin.users'))
         ->assertForbidden();
+});
+
+test("let's create a livewire component to list all users in the page", function () {
+    actingAs(User::factory()->admin()->create());
+
+    $users = User::factory()->count(10)->create();
+
+    $lw = Livewire::test(Users\Index::class);
+    $lw->assertSet('users', function ($users) {
+        expect($users)
+            ->toBeInstanceOf(LengthAwarePaginator::class)
+            ->toHaveCount(11);
+
+        return true;
+    });
+
+    foreach ($users as $user) {
+        $lw->assertSee($user->name);
+    }
 });
