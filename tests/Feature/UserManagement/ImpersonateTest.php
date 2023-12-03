5c012fe7d33f791e889f8e0214ad2e86bc3e2f44
Alex Nogueira <alex@devaction.com.br>
2023-12-03 10:11:58 -0300

    feat: Add HandleImpersonate middleware

--- tests/Feature/UserManagement/ImpersonateTest.php
+++ tests/Feature/UserManagement/ImpersonateTest.php
@@ -3,7 +3,7 @@
 use App\Livewire\Admin\Users\Impersonate;
 use App\Models\User;
 
-use function Pest\Laravel\actingAs;
+use function Pest\Laravel\{actingAs, get};
 use function PHPUnit\Framework\{assertSame, assertTrue};
 
 it('should add a key impersonate to the session with the given user', function () {
@@ -17,4 +17,21 @@ it('should add a key impersonate to the session with the given user', function (
     assertTrue(session()->has('impersonate'));
 
     assertSame(session()->get('impersonate'), $user->id);
+});
+
+it('should make sure that we are logged with impersonate user', function () {
+    $admin = User::factory()->admin()->create();
+    $user  = User::factory()->create();
+
+    actingAs($admin);
+
+    expect(auth()->user()?->id)->toBe($admin->id);
+
+    Livewire::test(Impersonate::class)
+        ->call('impersonate', $user->id);
+
+    get(route('welcome'))
+        ->assertSee(__("You're impersonating :name, click here to stop the impersonate.", ['name' => $user->name]));
+
+    expect(auth()->id())->toBe($user->id);
 });
