e593331df36fd6f9aae3c1e36510195757670863
Alex Nogueira <alex@devaction.com.br>
2023-12-04 08:33:47 -0300

    fix(app): fix impersonation stop button visibility

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -9,7 +9,9 @@
 </head>
 <body class="min-h-screen font-sans antialiased">
 <x-toast/>
-
+@if(session('impersonate'))
+    <livewire:admin.users.stop-impersonate/>
+@endif
 <x-main full-width>
     <x-slot:sidebar drawer="main-drawer" collapsible class="pt-3 bg-sky-800 text-white">
 
@@ -47,11 +49,6 @@
 
     <!-- The `$slot` goes here -->
     <x-slot:content>
-
-        @if(session('impersonate'))
-            {{ __("You're impersonating :name, click here to stop the impersonate.", ['name' => auth()->user()->name]) }}
-        @endif
-
         {{ $slot }}
     </x-slot:content>
 </x-main>
--- resources/views/livewire/admin/users/stop-impersonate.blade.php
+++ resources/views/livewire/admin/users/stop-impersonate.blade.php
@@ -1,7 +1,10 @@
-<div>
+<div class="bg-yellow-300 px-4 text-sm font-bold text-yellow-900">
+    {{ __("You're impersonating :name, click here", ['name' => auth()->user()->name]) }}
     <x-button
         icon="o-power"
-        class="btn-circle btn-ghost btn-xs"
+        class="btn-circle btn-error btn-sm"
         wire:click="stopImpersonate"
+
     />
+    {{ __("to stop impersonating.")}}
 </div>
