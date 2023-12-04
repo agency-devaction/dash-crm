75323ac4a46ba3cfabf77450fda05e860187ea68
Alex Nogueira <alex@devaction.com.br>
2023-12-04 08:17:01 -0300

    feat: Add StopImpersonate component and test

---
+++ app/Livewire/Admin/Users/StopImpersonate.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Livewire\Admin\Users;
+
+use Illuminate\Contracts\View\View;
+use Livewire\Component;
+
+class StopImpersonate extends Component
+{
+    public function render(): View
+    {
+        return view('livewire.admin.users.stop-impersonate', ['user' => auth()->user()]);
+    }
+
+    public function stopImpersonate(): void
+    {
+        session()->forget('impersonate');
+
+        $this->redirect(route('admin.users'));
+    }
+}
--- tests/Feature/UserManagement/ImpersonateTest.php
+++ tests/Feature/UserManagement/ImpersonateTest.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Livewire\Admin\Users\Impersonate;
+use App\Livewire\Admin\Users\{Impersonate, StopImpersonate};
 use App\Models\User;
 
 use function Pest\Laravel\{actingAs, get};
@@ -35,4 +35,28 @@ it('should make sure that we are logged with impersonate user', function () {
         ->assertSee(__("You're impersonating :name, click here to stop the impersonate.", ['name' => $user->name]));
 
     expect(auth()->id())->toBe($user->id);
+});
+
+it('should be able to stop impersonate', function () {
+    $admin = User::factory()->admin()->create();
+    $user  = User::factory()->create();
+
+    actingAs($admin);
+
+    expect(auth()->user()?->id)->toBe($admin->id);
+
+    Livewire::test(Impersonate::class)
+        ->call('impersonate', $user->id)
+        ->assertRedirect(route('welcome'));
+
+    Livewire::test(StopImpersonate::class)
+        ->call('stopImpersonate', $user->id)
+        ->assertRedirect(route('admin.users'));
+
+    expect(session('impersonate'))->toBeNull();
+
+    get(route('admin.dashboard'))
+        ->assertDontSeeLivewire(__("You're impersonating :name, click here to stop the impersonate.", ['name' => $user->name]));
+
+    expect(auth()->id())->toBe($admin->id);
 });
