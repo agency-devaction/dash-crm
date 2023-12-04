ed09adba1083fe41f16fa783fb5619e19e11c807
Alex Nogueira <alex@devaction.com.br>
2023-12-04 12:56:01 -0300

    fix: Prevent impersonating oneself

--- app/Livewire/Admin/Users/Impersonate.php
+++ app/Livewire/Admin/Users/Impersonate.php
@@ -5,6 +5,7 @@ namespace App\Livewire\Admin\Users;
 use App\Enum\Can;
 use Livewire\Attributes\On;
 use Livewire\Component;
+use RuntimeException;
 
 class Impersonate extends Component
 {
@@ -23,6 +24,11 @@ class Impersonate extends Component
     public function impersonate(int $id): void
     {
         $this->authorize(Can::BE_AN_ADMIN->value);
+
+        if (auth()->id() === $id) {
+            throw new RuntimeException('You cannot impersonate yourself.');
+        }
+
         session()->put('impersonator', auth()->id());
         session()->put('impersonate', $id);
 
--- tests/Feature/UserManagement/ImpersonateTest.php
+++ tests/Feature/UserManagement/ImpersonateTest.php
@@ -83,3 +83,12 @@ it('should have the correct permission to impersonate someone', function () {
         ->assertRedirect(route('welcome'));
 
 });
+
+it('should be able impossible to impersonate myself', function () {
+    $admin = User::factory()->admin()->create();
+
+    actingAs($admin);
+
+    Livewire::test(Impersonate::class)
+        ->call('impersonate', $admin->id);
+})->throws(RuntimeException::class);
